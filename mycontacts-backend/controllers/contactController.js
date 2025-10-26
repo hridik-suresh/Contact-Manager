0e5f9d97e6302a90c26640dcf9b3c7f8fccaf664
hridik-suresh <iamhridiksuresh@gmail.com>
2025-10-26 23:28:52 +0530

    change all the responses to json data

--- mycontacts-backend/controllers/contactController.js
+++ mycontacts-backend/controllers/contactController.js
@@ -2,35 +2,35 @@
 //@route GET /api/contacts
 //@access Public
 const getContacts = (req, res) => {
-  res.status(200).send('view all contacts');
+  res.status(200).json({ message: 'view all contacts' });
 }
 
 //@desc Create new contact
 //@route POST /api/contacts
 //@access Public
 const createContact = (req, res) => {
-  res.status(201).send('create new contact');
+  res.status(201).json({ message: "create new contact" });
 }
 
 //@desc Get contact by ID
 //@route GET /api/contacts/:id
 //@access Public
 const getContactById = (req, res) => {
-  res.status(200).send(`view contact with id ${req.params.id}`);
+  res.status(200).json({ message: `view contact with id ${req.params.id}` });
 }
 
 //@desc Update contact
 //@route PUT /api/contacts/:id
 //@access Public
 const updateContact = (req, res) => {
-  res.status(200).send(`update contact with id ${req.params.id}`);
+  res.status(200).json({ message: `update contact with id ${req.params.id}` });
 }
 
 //@desc Delete contact
 //@route DELETE /api/contacts/:id
 //@access Public
 const deleteContact = (req, res) => {
-  res.status(200).send(`delete contact with id ${req.params.id}`);
+  res.status(200).json({ message: `delete contact with id ${req.params.id}` });
 }
 
 module.exports = {
--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -4,9 +4,14 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Middleware------------
+app.use(express.json());// Middleware to parse JSON bodies
+
+// Routes------------
 
 app.use('/api/contacts', require('./routes/contactRoutes'));
 
+// Start the server------------
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
