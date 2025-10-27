b445181b3da6121605093114ba06ab0c3858d566
hridik-suresh <iamhridiksuresh@gmail.com>
2025-10-27 11:14:54 +0530

    make use of "express-async-handler" npm package instead of usual try..catch for error handling in asyncronous functions.

--- mycontacts-backend/controllers/contactController.js
+++ mycontacts-backend/controllers/contactController.js
@@ -1,42 +1,44 @@
+const asyncHandler = require('express-async-handler');
+
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access Public
-const getContacts = (req, res) => {
+const getContacts = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'view all contacts' });
-}
+});
 
 //@desc Create new contact
 //@route POST /api/contacts
 //@access Public
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
   const { name, email, phone } = req.body;
   if (!name || !email || !phone) {
     res.status(400);
     throw new Error("All fields are mandatory!");
   }
   res.status(201).json({ message: "create new contact" });
-}
+});
 
 //@desc Get contact by ID
 //@route GET /api/contacts/:id
 //@access Public
-const getContactById = (req, res) => {
+const getContactById = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `view contact with id ${req.params.id}` });
-}
+});
 
 //@desc Update contact
 //@route PUT /api/contacts/:id
 //@access Public
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `update contact with id ${req.params.id}` });
-}
+});
 
 //@desc Delete contact
 //@route DELETE /api/contacts/:id
 //@access Public
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `delete contact with id ${req.params.id}` });
-}
+});
 
 module.exports = {
   getContacts,
