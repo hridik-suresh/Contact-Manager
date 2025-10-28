5e8c146a17c050feed81da976c7374e90538be49
hridik-suresh <iamhridiksuresh@gmail.com>
2025-10-28 15:57:56 +0530

    the crud operation is completed

--- mycontacts-backend/controllers/contactController.js
+++ mycontacts-backend/controllers/contactController.js
@@ -18,28 +18,55 @@ const createContact = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("All fields are mandatory!");
   }
-  res.status(201).json({ message: "create new contact" });
+  const contact = await Contact.create({
+    name,
+    email,
+    phone
+  });
+  res.status(201).json(contact);
 });
 
 //@desc Get contact by ID
 //@route GET /api/contacts/:id
 //@access Public
 const getContactById = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `view contact with id ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+  res.status(200).json(contact);
 });
 
 //@desc Update contact
 //@route PUT /api/contacts/:id
 //@access Public
 const updateContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `update contact with id ${req.params.id}` });
+  const { name, email, phone } = req.body;
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+  const updatedContact = await Contact.findByIdAndUpdate(
+    req.params.id,
+    { name, email, phone },
+    { runValidators: true, new: true }
+  );
+  res.status(200).json(updatedContact);
 });
 
 //@desc Delete contact
 //@route DELETE /api/contacts/:id
 //@access Public
 const deleteContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `delete contact with id ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+  await Contact.findByIdAndDelete(req.params.id);
+  res.status(200).json(contact);
 });
 
 module.exports = {
