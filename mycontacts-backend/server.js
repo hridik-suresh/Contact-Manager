9c769fb8c0956a537f49d9837fe4911be595e30c
hridik-suresh <iamhridiksuresh@gmail.com>
2025-10-28 15:09:44 +0530

    established the connection between the nodejs and mongodb by using mongoose

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -1,9 +1,23 @@
 const express = require('express');
-const dotenv = require('dotenv').config();
 const app = express();
+const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
 const { errorHandler } = require('./middlewares/errorHandler');
 
 const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB------------
+async function main() {
+   await mongoose.connect('mongodb://127.0.0.1:27017/mycontactsDB');
+}
+
+
+main()
+    .then(() =>{
+        console.log("connected to MongoDB");
+    })
+    .catch((err)=> console.log(err));
+
 
 // Middleware------------
 app.use(express.json());// Middleware to parse JSON bodies
