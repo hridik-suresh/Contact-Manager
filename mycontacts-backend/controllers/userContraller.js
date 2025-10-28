813d06af7d950777969ca48b5e7adf6bac152df8
hridik-suresh <iamhridiksuresh@gmail.com>
2025-10-28 20:00:57 +0530

    registering user is successful

--- mycontacts-backend/controllers/userContraller.js
+++ mycontacts-backend/controllers/userContraller.js
@@ -1,12 +1,42 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
 const User = require("../models/userModel");
 
 //@desc Register new user
 //@route POST /api/user/register
 //@access Public
 const registerUser = asyncHandler(async (req, res) => {
-// Registration logic here
-    res.status(201).send('User registered');
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+        res.status(400);
+        throw new Error('Please add all fields');
+    }
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    });
+    if (user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
+
 });
 
 //@desc Login user
