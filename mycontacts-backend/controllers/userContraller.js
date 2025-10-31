14aeae2a512203dbc6b165fc8dfbdcad5bae62c2
hridik-suresh <iamhridiksuresh@gmail.com>
2025-10-31 18:16:59 +0530

    jwt token sending during login is created

--- mycontacts-backend/controllers/userContraller.js
+++ mycontacts-backend/controllers/userContraller.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 //@desc Register new user
@@ -44,7 +45,23 @@ const registerUser = asyncHandler(async (req, res) => {
 //@access Public
 const loginUser = asyncHandler(async (req, res) => {
     // Login logic here
-    res.status(200).send('User logged in');
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(400);
+        throw new Error('Please add all fields');
+    }
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign(
+            { user: { name: user.name, email: user.email, id: user._id } },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '15m' }
+        );
+        res.status(200).json({ accessToken });
+    } else {
+        res.status(401);
+        throw new Error('Invalid credentials');
+    }
 });
 
 //@desc Get current user info
